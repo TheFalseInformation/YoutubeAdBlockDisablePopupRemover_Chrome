867798f5b67dea8ff16c2104956ec9f0fba5baa3
TheFalseInformation <144592429+TheFalseInformation@users.noreply.github.com>
2023-10-15 21:23:19 +0200

    Update ext.js

--- ext.js
+++ ext.js
@@ -1,13 +1,11 @@
 chrome.webNavigation.onCommitted.addListener(function(details) {
   // Das Element wird mithilfe des DOM-Selectors gefunden
-  const element = document.querySelector(".style-scope ytd-enforcement-message-view-model");
-  const element1 = document.querySelector("tp-yt-paper-dialog");
+  document.getElementById('masthead-ad').remove();
+  //const element1 = document.querySelector("div.style-scope ytd-video-masthead-ad-v3-renderer");
+  const element2 = document.querySelector("div.style-scope ytd-popup-container");
 
   // Das Element wird gelöscht
-  if (element) {
-    element.remove();
-  }
-  if (element1) {
-    element1.remove();
-  }
+  element.parentElement.removeChild(element);
+  //element1.parentElement.removeChild(element1);
+  element2.parentElement.removeChild(element2);
 });
